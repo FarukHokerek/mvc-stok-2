fcc3aa829b4ed4939f72c6bc41da14f4c6b5249f
f-hok <f-hok@DESKTOP-HUF9QA8>
2022-03-11 16:13:38 +0300

    Add project files.

---
+++ mvc stok 2/Controllers/SatisController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using mvc_stok_2.Models.Entity;
+
+namespace mvc_stok_2.Controllers
+{
+    public class SatisController : Controller
+    {
+        // GET: Satis
+        mvcdbstockEntities db = new mvcdbstockEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult YeniSatis()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniSatis(TBLSATISLAR p)
+        {
+            db.TBLSATISLAR.Add(p);
+            db.SaveChanges();
+            return View("Index");
+        }
+        
+    }
+}
